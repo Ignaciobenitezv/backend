4f642888fdd0d8d0ecf8aeedf3d4a5afa964dee0
ignaciobenitezv <benitezvucasignacio@gmail.com>
2023-03-15 17:32:00 +0100

    creandodb

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ const app= express();
 
 app.use(morgan('dev'))
 app.use(taskRoutes);
-app.listen(3000)
-console.log('Server on port 3000')
+app.listen(4000)
+console.log('Server on port 4000')
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -2,8 +2,20 @@ const {Router} = require('express');
 
 const router = Router();
 
-router.get('/', (req, res)=>{
-    res.send('Helo world');
+router.get('/tasks', (req, res)=>{
+    res.send('Retrieving a list of task');
+})
+router.get('/tasks/10', (req, res)=>{
+    res.send('Retrieving a list of task');
+})
+router.delete('/tasks', (req, res)=>{
+    res.send('Deleting a task');
+})
+router.post('/tasks', (req, res)=>{
+    res.send('Creating a task');
+})
+router.delete('/tasks', (req, res)=>{
+    res.send('Updating a task');
 })
 
 module.exports = router;
